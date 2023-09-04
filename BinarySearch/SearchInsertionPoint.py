593bc11a5d4fda9fa274ce1e66a0b35c7cc299da
Priyanka Bhosale <bhosale.priyanka.14it1037@gmail.com>
2023-09-04 16:57:40 -0400

    Create SearchInsertionPoint.py

---
+++ BinarySearch/SearchInsertionPoint.py
@@ -0,0 +1,16 @@
+#Same as LowerBound
+
+def lowerBound(arr: [int], n: int, x: int) -> int:
+    # Write your code here
+    l = 0
+    r = n-1
+    ans = n
+    while l<=r:
+        mid = (l+r)//2
+
+        if arr[mid]>=x:
+            ans = mid
+            r = mid -1
+        else:
+            l = mid +1
+    return ans
