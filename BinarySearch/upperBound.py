003748a301396d60b9ac9efce00436e71e8f7599
Priyanka Bhosale <bhosale.priyanka.14it1037@gmail.com>
2023-09-05 21:03:35 -0400

    Create upperBound.py

---
+++ BinarySearch/upperBound.py
@@ -0,0 +1,16 @@
+def upperBound(arr: [int], x: int, n: int) -> int:
+    # Write your code here.
+    l =0
+    r = n-1
+    ans = n
+
+    while l<=r:
+        mid = (l+r)//2
+
+        if arr[mid]>x:
+            ans = mid
+            r = mid-1
+        else:
+            l = l+1
+    
+    return ans
